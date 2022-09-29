4ba50f10e8110e17241bd7dee517b5cea3143a1e
Mohammad Shazny <shaznykd@gmail.com>
2022-09-29 16:58:02 +0530

    Ready For Students to check

---
+++ Batch17_WebApp/Models/ReturnModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Batch17_WebApp.Models
+{
+    public class ReturnModel
+    {
+        [EnumDataType(typeof(Status))]
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public object Model { get; set; }
+    }
+    public enum ReturnStatus
+    {
+        success,
+        failed,
+        NoData,
+        dataIssue,
+        serverFailed,
+        error
+    }
+}
